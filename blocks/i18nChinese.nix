85497b26860cbd7f5f32b490be6b3af24ea35c43
Shinonome Yuugata <shinonomeyuugata@gmail.com>
2024-11-30 19:51:27 +0800

    update

--- blocks/i18nChinese.nix
+++ blocks/i18nChinese.nix
@@ -7,4 +7,21 @@
     noto-fonts-cjk-serif
     noto-fonts-emoji
   ];
+  i18n = {
+    supportedLocales = [
+      "zh_CN.UTF-8/UTF-8"
+    ];
+    inputMethod = {
+      type = "fcitx5";
+      enable = true;
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-chinese-addons
+          fcitx5-mozc
+          fcitx5-gtk 
+          fcitx5-material-color
+        ];
+      };
+    };
+  };
 }
